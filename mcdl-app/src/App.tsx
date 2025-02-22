311b5a64bd9d4cae3a4a934da38c8269379a2fa2
Lok Leoi <lyulelok@gmail.com>
2025-02-22 01:15:08 -0500

    added sign in form

--- mcdl-app/src/App.tsx
+++ mcdl-app/src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home"
+import Auth from "./components/Auth";
 import './App.css';
 
 const App: React.FC = () => {
@@ -13,6 +14,7 @@ const App: React.FC = () => {
 
                 <Routes>
                     <Route path="/" element={<Home/>}/>
+                    <Route path="/auth" element={<Auth/>}/>
                 </Routes>
 
 
