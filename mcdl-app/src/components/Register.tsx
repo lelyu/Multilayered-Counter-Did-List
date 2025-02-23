479cc80373d180318696f52e9079d99d24c38f0b
lok-lyu <lyulelok@gmail.com>
2025-02-23 10:52:19 -0500

    update

--- mcdl-app/src/components/Register.tsx
+++ mcdl-app/src/components/Register.tsx
@@ -20,17 +20,17 @@ const Register: React.FC = () => {
         event.preventDefault();
 
         createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed up
-            const user = userCredential.user;
-            // ...
-            window.location.href = '/login';
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ..
-        });
+            .then((userCredential) => {
+                // Signed up
+                const user = userCredential.user;
+                // ...
+                window.location.href = '/';
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+            });
 
     };
 
