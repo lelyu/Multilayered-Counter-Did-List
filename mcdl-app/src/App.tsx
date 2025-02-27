705083dcff59f26177636149d45bd445ba8e1688
lok-lyu <lyulelok@gmail.com>
2025-02-27 17:21:13 -0500

    new card components for detailed page

--- mcdl-app/src/App.tsx
+++ mcdl-app/src/App.tsx
@@ -1,28 +1,30 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./components/Home"
+import Home from "./components/Home";
 import Register from "./components/Register.tsx";
 import Login from "./components/Login.tsx";
-import './App.css';
+import "./App.css";
+import FolderCard from "./components/FolderCard.tsx";
 
 const App: React.FC = () => {
-    return (
-        <div className="app-container shadow p-3 mb-5 bg-body-tertiary rounded">
-            <Router>
-                <Navbar/>
+	return (
+		<div className="app-container shadow p-3 mb-5 bg-body-tertiary rounded">
+			<Router>
+				<Navbar />
 
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/register" element={<Register/>}/>
-                    <Route path="/login" element={<Login/>}/>
-                </Routes>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/card" element={<FolderCard />} />
+				</Routes>
 
-                <Footer/>
-            </Router>
-        </div>
-    );
+				<Footer />
+			</Router>
+		</div>
+	);
 };
 
 export default App;
