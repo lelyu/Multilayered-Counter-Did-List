abe37449f182ed181fa65002bc8fafd72720f673
lok-lyu <lyulelok@gmail.com>
2025-03-05 13:19:53 -0500

    update descriptions

--- mcdl-app/src/components/Scrollspy.tsx
+++ mcdl-app/src/components/Scrollspy.tsx
@@ -5,25 +5,31 @@ const Scrollspy = () => {
 				<div id="list-example" className="list-group">
 					<a
 						className="list-group-item list-group-item-action"
-						href="#list-item-1"
+						href={"#intro"}
+					>
+						DocIt
+					</a>
+					<a
+						className="list-group-item list-group-item-action"
+						href={"#folder"}
 					>
 						Folder
 					</a>
 					<a
 						className="list-group-item list-group-item-action"
-						href="#list-item-2"
+						href={"#list"}
 					>
 						List
 					</a>
 					<a
 						className="list-group-item list-group-item-action"
-						href="#list-item-3"
+						href={"#list-item"}
 					>
 						List Item
 					</a>
 					<a
 						className="list-group-item list-group-item-action"
-						href="#list-item-4"
+						href={"#ai"}
 					>
 						AI
 					</a>
@@ -42,7 +48,28 @@ const Scrollspy = () => {
 						position: "relative",
 					}}
 				>
-					<h4 id="list-item-1">Folder</h4>
+					<h4 id="intro">Doc it? It docs!</h4>
+					<p>
+						Our attention span is getting shorter, and we're getting
+						busier. A lot of times, I couldn't remember what I ate
+						for breakfast!
+					</p>
+					<p>
+						DocIt solves this kind of problem in workplace. Imagine
+						you're asked to submit a monthly report by the end of
+						today. I bet you can't remember all the details --
+						details that matter!
+					</p>
+					<p>
+						DocIt is a new way to document and organize your work
+						journal. You just need to enter a few things, and DocIt
+						will handle the rest. You could even use AI to write a
+						work report for you. This time, you won't miss out on
+						any important details, because DocIt meticulously checks
+						your day to day work, acting like an unbiased assistant.
+					</p>
+
+					<h4 id="folder">Folder</h4>
 					<p>
 						The folder feature in the app serves as a robust
 						organizational tool, enabling you to group and store
@@ -54,7 +81,7 @@ const Scrollspy = () => {
 						categories, as it ensures that everything is easy to
 						locate and access.
 					</p>
-					<h4 id="list-item-2">List</h4>
+					<h4 id="list">List</h4>
 					<p>
 						The list component acts as a central hub for managing
 						tasks, items, or records within the app. Designed with
@@ -64,7 +91,7 @@ const Scrollspy = () => {
 						functionality, so as you scroll through different
 						sections, the corresponding list entry is highlighted.
 					</p>
-					<h4 id="list-item-3">List Item</h4>
+					<h4 id="list-item">List Item</h4>
 					<p>
 						Each list item represents an individual entry within
 						your larger list, providing detailed information and
@@ -74,7 +101,7 @@ const Scrollspy = () => {
 						for precise control over your data.
 					</p>
 
-					<h4 id="list-item-4">AI</h4>
+					<h4 id="ai">AI</h4>
 					<p>
 						The AI feature is the app’s intelligent assistant,
 						designed to augment your productivity by offering smart
